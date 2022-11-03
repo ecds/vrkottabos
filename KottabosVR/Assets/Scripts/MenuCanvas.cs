55172e3a9ba244317bbd45cd9132d681ae4f8b21
DonovanLott <93689796+DonovanLott@users.noreply.github.com>
2022-11-02 21:50:41 -0400

    Donovan Main Menu
    Donovan made main menu

---
+++ KottabosVR/Assets/Scripts/MenuCanvas.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuCanvas : MonoBehaviour
+{
+    GameObject mainMenuCanvas;
+    GameObject levelSelectCanvas;
+    GameObject settingsCanvas;
+    GameObject creditsCanvas;
+
+    GameObject currentCanvas;
+
+    
+    void Start()
+    {
+        mainMenuCanvas = GameObject.Find("MainMenuCanvas");
+        mainMenuCanvas.SetActive(true);
+        levelSelectCanvas = GameObject.Find("LevelSelectCanvas");
+        levelSelectCanvas.SetActive(false);
+        settingsCanvas = GameObject.Find("SettingsCanvas");
+        settingsCanvas.SetActive(false);
+        creditsCanvas = GameObject.Find("CreditsCanvas");
+        creditsCanvas.SetActive(false);
+    }
+
+
+    public void BackToMenu()
+    {
+        currentCanvas = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
+        currentCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+    }
+
+    public void LoadLevelSelectCanvas()
+    {
+        currentCanvas = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
+        currentCanvas.SetActive(false);
+        levelSelectCanvas.SetActive(true);
+    }
+
+    public void LoadSettingsCanvas()
+    {
+        currentCanvas = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
+        currentCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+    }
+
+    public void LoadCreditsCanvas()
+    {
+        currentCanvas = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
+        currentCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+
+}
